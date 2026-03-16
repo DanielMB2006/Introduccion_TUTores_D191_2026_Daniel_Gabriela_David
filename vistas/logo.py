25714700eccef3a2d35c8d50950e704fe614ebe7
Daniel Monsalve <dannymonsalveb@gmail.com>
2026-03-15 19:56:15 -0500

    actualizar logo

--- vistas/logo.py
+++ vistas/logo.py
@@ -1,12 +1,11 @@
 import streamlit as st
+import os
 
 def mostrar_logo():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    logo_path = os.path.join(base_dir, "..", "imagenes", "logo.PNG")
 
-    col1, col2, col3 = st.columns([1,2,1])
+    col1, col2, col3 = st.columns([1, 2, 1])
 
     with col2:
-
-        st.image(
-            "imagenes/logo.png",
-            width=200
-        )
+        st.image(logo_path, width=200)
